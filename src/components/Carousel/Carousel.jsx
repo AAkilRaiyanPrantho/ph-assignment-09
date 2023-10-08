db6d1d1cfd5dc3dd0d9b4fd210612b2515b2afe7
MDAakilRaiyan <aakil15-11060@diu.edu.bd>
2023-10-08 18:32:30 +0600

    Carousel mixed with Hero

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -2,41 +2,176 @@ const Carousel = () => {
   return (
     <div>
       <div className="carousel max-lg:flex max-lg:mx-auto max-lg:w-5/6 max-lg:h-[350px] lg:w-full h-[700px] ">
-        <div id="item1" className="carousel-item w-full">
+        {/* <div id="item1" className="carousel-item w-full">
           <img
             src="https://i.ibb.co/YLKHwXN/flatten.webp"
             className="w-full"
           />
+        </div> */}
+        <div
+          id="item1"
+          className="hero carousel-item w-full"
+          style={{
+            backgroundImage:
+              "url(https://i.ibb.co/YLKHwXN/flatten.webp)",
+          }}
+        >
+          <div className="hero-overlay bg-opacity-60"></div>
+          <div className="hero-content text-center text-neutral-content">
+            <div className="max-w-md">
+              <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+              <p className="mb-5">
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                assumenda excepturi exercitationem quasi. In deleniti eaque aut
+                repudiandae et a id nisi.
+              </p>
+              <button className="btn btn-primary">Get Started</button>
+            </div>
+          </div>
         </div>
-        <div id="item2" className="carousel-item w-full">
+
+        {/* <div id="item2" className="carousel-item w-full">
           <img
             src="https://i.ibb.co/2tmFZYS/13038-Musical-theatre-students-performing.jpg"
             className="w-full"
           />
+        </div> */}
+
+        <div
+          id="item2"
+          className="hero carousel-item w-full"
+          style={{
+            backgroundImage:
+              "url(https://i.ibb.co/2tmFZYS/13038-Musical-theatre-students-performing.jpg)",
+          }}
+        >
+          <div className="hero-overlay bg-opacity-60"></div>
+          <div className="hero-content text-center text-neutral-content">
+            <div className="max-w-md">
+              <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+              <p className="mb-5">
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                assumenda excepturi exercitationem quasi. In deleniti eaque aut
+                repudiandae et a id nisi.
+              </p>
+              <button className="btn btn-primary">Get Started</button>
+            </div>
+          </div>
         </div>
-        <div id="item3" className="carousel-item w-full">
+
+        {/* <div id="item3" className="carousel-item w-full">
           <img
             src="https://i.ibb.co/zxXHFyV/indie-film-festival-premiere.jpg"
             className="w-full"
           />
+        </div> */}
+
+        <div
+          id="item3"
+          className="hero carousel-item w-full"
+          style={{
+            backgroundImage:
+              "url(https://i.ibb.co/zxXHFyV/indie-film-festival-premiere.jpg)",
+          }}
+        >
+          <div className="hero-overlay bg-opacity-60"></div>
+          <div className="hero-content text-center text-neutral-content">
+            <div className="max-w-md">
+              <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+              <p className="mb-5">
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                assumenda excepturi exercitationem quasi. In deleniti eaque aut
+                repudiandae et a id nisi.
+              </p>
+              <button className="btn btn-primary">Get Started</button>
+            </div>
+          </div>
         </div>
-        <div id="item4" className="carousel-item w-full">
+
+        {/* <div id="item4" className="carousel-item w-full">
           <img
             src="https://i.ibb.co/zsSBNQv/All-Winners-Confetti-1024x495.jpg"
             className="w-full"
           />
+        </div> */}
+
+        <div
+          id="item4"
+          className="hero carousel-item w-full"
+          style={{
+            backgroundImage:
+              "url(https://i.ibb.co/zsSBNQv/All-Winners-Confetti-1024x495.jpg)",
+          }}
+        >
+          <div className="hero-overlay bg-opacity-60"></div>
+          <div className="hero-content text-center text-neutral-content">
+            <div className="max-w-md">
+              <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+              <p className="mb-5">
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                assumenda excepturi exercitationem quasi. In deleniti eaque aut
+                repudiandae et a id nisi.
+              </p>
+              <button className="btn btn-primary">Get Started</button>
+            </div>
+          </div>
         </div>
-        <div id="item5" className="carousel-item w-full">
+
+        {/* <div id="item5" className="carousel-item w-full">
           <img
             src="https://i.ibb.co/fdNyctG/AAAAQSGq-RNATRmf-Lr-z-YKppk-Qg-KLn-Z4ao-I9v-NStx-NYs-U6n-Cm-E8d6-D8-Vq-M25un-ZQmv6572tp2-Hboa-Ikx1-Q.jpg"
             className="w-full"
           />
+        </div> */}
+
+        <div
+          id="item5"
+          className="hero carousel-item w-full"
+          style={{
+            backgroundImage:
+              "url(https://i.ibb.co/fdNyctG/AAAAQSGq-RNATRmf-Lr-z-YKppk-Qg-KLn-Z4ao-I9v-NStx-NYs-U6n-Cm-E8d6-D8-Vq-M25un-ZQmv6572tp2-Hboa-Ikx1-Q.jpg)",
+          }}
+        >
+          <div className="hero-overlay bg-opacity-60"></div>
+          <div className="hero-content text-center text-neutral-content">
+            <div className="max-w-md">
+              <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+              <p className="mb-5">
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                assumenda excepturi exercitationem quasi. In deleniti eaque aut
+                repudiandae et a id nisi.
+              </p>
+              <button className="btn btn-primary">Get Started</button>
+            </div>
+          </div>
         </div>
-        <div id="item6" className="carousel-item w-full">
+
+        {/* <div id="item6" className="carousel-item w-full">
           <img
             src="https://i.ibb.co/ThjPQrM/Sports-events-insurance-Main-scaled-e1644805433386.jpg"
             className="w-full"
           />
+        </div> */}
+        <div
+          id="item6"
+          className="hero carousel-item w-full"
+          style={{
+            backgroundImage:
+              "url(https://i.ibb.co/ThjPQrM/Sports-events-insurance-Main-scaled-e1644805433386.jpg)",
+          }}
+        >
+          <div className="hero-overlay bg-opacity-60"></div>
+          <div className="hero-content text-center text-neutral-content">
+            <div className="max-w-md">
+              <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+              <p className="mb-5">
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                assumenda excepturi exercitationem quasi. In deleniti eaque aut
+                repudiandae et a id nisi.
+              </p>
+              <button className="btn btn-primary">Get Started</button>
+            </div>
+          </div>
         </div>
       </div>
       <div className="flex justify-center w-full py-2 gap-2">
